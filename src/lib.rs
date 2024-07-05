0523983c1e81d094fd8991f55007c5afc1d89a21
TheKnightGawain <github.com.nugget057@passmail.net>
2024-07-05 15:30:29 +0930

    Added new_from_closure

--- src/lib.rs
+++ src/lib.rs
@@ -126,6 +126,26 @@ impl<T> Matrix<T> where T: Copy {
             columns,
         })
     }
+
+    pub fn new_from_closure(
+        f: fn(i: usize, j: usize) -> T,
+        rows: usize,
+        columns: usize
+    ) -> Result<Self, SizingError> {
+        if rows == 0 && columns != 0 {
+            return Err(SizingError::Row(0));
+        }
+
+        if columns == 0 && rows != 0 {
+            return Err(SizingError::Column(0));
+        }
+
+        let data = (0..rows)
+            .flat_map(|i| (0..columns).map(|j| f(i, j)).collect::<Vec<_>>())
+            .collect();
+
+        Ok(Self { data, rows, columns })
+    }
 }
 
 // getters
@@ -1338,6 +1358,63 @@ mod tests {
                         rows: 0,
                         columns: 0,
                     })
+                );
+            }
+        }
+
+        mod new_from_closure {
+            use super::*;
+
+            #[test]
+            fn handles_errors() {
+                assert_eq!(
+                    Matrix::new_from_closure(|i, _j| i, 0, 6),
+                    Err(SizingError::Row(0))
+                );
+                assert_eq!(
+                    Matrix::new_from_closure(|_i, j| j, 13, 0),
+                    Err(SizingError::Column(0))
+                );
+            }
+
+            #[test]
+            fn creates_matrix() {
+                assert_eq!(
+                    Matrix::new_from_closure(|i, j| i + j, 4, 6),
+                    Ok(Matrix {
+                        data: vec![
+                            0,
+                            1,
+                            2,
+                            3,
+                            4,
+                            5,
+                            1,
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            8
+                        ],
+                        rows: 4,
+                        columns: 6,
+                    })
+                );
+                assert_eq!(
+                    Matrix::new_from_closure(|i, j| i * j, 0, 0),
+                    Ok(Matrix::empty())
                 );
             }
         }
