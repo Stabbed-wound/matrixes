fcc433951d9967874c2cdca4047faa87ab4868ee
GavGav <github.com.easter342@passmail.net>
2025-03-23 08:17:44 +1030

    Added swappers

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use num_traits::{One, Zero};
-use std::array;
 use std::ops::{Index, IndexMut};
+use std::{array, mem};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Matrix<T, const R: usize, const C: usize>([[T; C]; R]);
@@ -172,6 +172,52 @@ impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
                     .collect::<Option<Vec<&mut T>>>()
             })
             .collect::<Option<Vec<Vec<&mut T>>>>()
+    }
+}
+
+// swappers
+
+impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
+    pub fn swap_rows(&mut self, row1: usize, row2: usize) {
+        if row1 == row2 {
+            return;
+        }
+
+        let self_ptr = self as *mut Self;
+
+        let Some(row1) = self.get_mut_row(row1) else {
+            return;
+        };
+        // Safety
+        // row1 and row2 are different so this is safe
+        let Some(row2) = unsafe { &mut *self_ptr }.get_mut_row(row2) else {
+            return;
+        };
+
+        row1.into_iter()
+            .zip(row2)
+            .for_each(|(elem1, elem2)| mem::swap(elem1, elem2));
+    }
+
+    pub fn swap_cols(&mut self, col1: usize, col2: usize) {
+        if col1 == col2 {
+            return;
+        }
+
+        let self_ptr = self as *mut Self;
+
+        let Some(col1) = self.get_mut_col(col1) else {
+            return;
+        };
+        // Safety
+        // col1 and col2 are different so this is safe.
+        let Some(col2) = unsafe { &mut *self_ptr }.get_mut_col(col2) else {
+            return;
+        };
+
+        col1.into_iter()
+            .zip(col2)
+            .for_each(|(elem1, elem2)| mem::swap(elem1, elem2));
     }
 }
 
