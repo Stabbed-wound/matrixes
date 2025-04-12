b90873295c2a7a70ea9d4b7520f7c60a16d7d9dd
GavGav <github.com.easter342@passmail.net>
2025-04-12 15:36:50 +0930

    Added unchecked variants of getters

--- src/accessors.rs
+++ src/accessors.rs
@@ -11,7 +11,11 @@ impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
     pub const fn size(&self) -> usize {
         R * C
     }
+}
 
+// get
+
+impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
     /// # Errors
     /// - row must index within bounds
     /// - col must index within bounds
@@ -89,6 +93,55 @@ impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
             .collect::<Result<Vec<_>, IndexError>>()
     }
 }
+
+// get_unchecked
+
+impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
+    pub const fn get_unchecked(&self, row: usize, col: usize) -> &T {
+        &self.0[row][col]
+    }
+
+    pub fn get_row_unchecked(&self, row: usize) -> [&T; C] {
+        array::from_fn(|col| &self.0[row][col])
+    }
+
+    pub fn get_col_unchecked(&self, col: usize) -> [&T; R] {
+        array::from_fn(|row| &self.0[row][col])
+    }
+
+    pub fn get_rows_unchecked<I>(&self, rows: I) -> Vec<[&T; C]>
+    where
+        I: IntoIterator<Item = usize>,
+    {
+        rows.into_iter()
+            .map(|row| self.get_row_unchecked(row))
+            .collect()
+    }
+
+    pub fn get_cols_unchecked<I>(&self, cols: I) -> Vec<[&T; R]>
+    where
+        I: IntoIterator<Item = usize>,
+    {
+        cols.into_iter()
+            .map(|col| self.get_col_unchecked(col))
+            .collect()
+    }
+
+    pub fn get_area_unchecked<I1, I2>(&self, rows: I1, cols: I2) -> Vec<Vec<&T>>
+    where
+        I1: IntoIterator<Item = usize>,
+        I2: IntoIterator<Item = usize>,
+        I2::IntoIter: Clone,
+    {
+        let cols = cols.into_iter();
+
+        rows.into_iter()
+            .map(|row| cols.clone().map(|col| &self.0[row][col]).collect())
+            .collect()
+    }
+}
+
+// get_mut
 
 impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
     /// # Errors
@@ -213,16 +266,83 @@ impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
     }
 }
 
+// get_unchecked_mut
+
+impl<T, const R: usize, const C: usize> Matrix<T, R, C> {
+    pub const fn get_unchecked_mut(&mut self, row: usize, col: usize) -> &mut T {
+        &mut self.0[row][col]
+    }
+
+    pub fn get_row_unchecked_mut(&mut self, row: usize) -> [&mut T; C] {
+        let self_ptr = self as *mut Self;
+
+        array::from_fn(|col| &mut unsafe { &mut *self_ptr }.0[row][col])
+    }
+
+    pub fn get_col_unchecked_mut(&mut self, col: usize) -> [&mut T; R] {
+        let self_ptr = self as *mut Self;
+
+        array::from_fn(|row| &mut unsafe { &mut *self_ptr }.0[row][col])
+    }
+
+    /// # Safety
+    /// rows must have no duplicates
+    pub unsafe fn get_rows_unchecked_mut<I>(&mut self, rows: I) -> Vec<[&mut T; C]>
+    where
+        I: IntoIterator<Item = usize>,
+    {
+        let self_ptr = self as *mut Self;
+
+        rows.into_iter()
+            .map(|row| unsafe { &mut *self_ptr }.get_row_unchecked_mut(row))
+            .collect()
+    }
+
+    /// # Safety
+    /// cols must have no duplicates
+    pub unsafe fn get_cols_unchecked_mut<I>(&mut self, cols: I) -> Vec<[&mut T; R]>
+    where
+        I: IntoIterator<Item = usize>,
+    {
+        let self_ptr = self as *mut Self;
+
+        cols.into_iter()
+            .map(|col| unsafe { &mut *self_ptr }.get_col_unchecked_mut(col))
+            .collect()
+    }
+
+    /// # Safety
+    /// - rows must have no duplicates
+    /// - cols must have no duplicates
+    pub unsafe fn get_area_unchecked_mut<I1, I2>(&mut self, rows: I1, cols: I2) -> Vec<Vec<&mut T>>
+    where
+        I1: IntoIterator<Item = usize>,
+        I2: IntoIterator<Item = usize>,
+        I2::IntoIter: Clone,
+    {
+        let self_ptr = self as *mut Self;
+        let cols = cols.into_iter();
+
+        rows.into_iter()
+            .map(|row| {
+                cols.clone()
+                    .map(|col| &mut unsafe { &mut *self_ptr }.0[row][col])
+                    .collect()
+            })
+            .collect()
+    }
+}
+
 impl<T, const R: usize, const C: usize> Index<(usize, usize)> for Matrix<T, R, C> {
     type Output = T;
 
     fn index(&self, (row, col): (usize, usize)) -> &Self::Output {
-        &self.0[row][col]
+        self.get_unchecked(row, col)
     }
 }
 
 impl<T, const R: usize, const C: usize> IndexMut<(usize, usize)> for Matrix<T, R, C> {
     fn index_mut(&mut self, (row, col): (usize, usize)) -> &mut Self::Output {
-        &mut self.0[row][col]
+        self.get_unchecked_mut(row, col)
     }
 }
